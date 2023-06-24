d7acb667524bc0b692ac474d1ac43c8a9fe515e6
Nightmare8 <juan.caniguante@sansano.usm.cl>
2023-06-24 12:00:12 -0400

    fix some problems with grid

--- src/components/seleccionado.jsx
+++ src/components/seleccionado.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useRef} from 'react'
 
 import './../css/seleccionado.css'
 
@@ -6,16 +6,27 @@ function Seleccionado() {
 
     const [seleccionado, setSeleccionado] = useState('AMBIENTAL');
 
+    const refBotones = useRef()
+
+    const removeActive = () =>{
+        refBotones.current.childNodes.forEach((e)=>{
+            e.classList.remove('active')
+        })
+    }
     const changeSeleccionado = (e) => {
-        console.log(e.target)
+        if (!e.target.className.includes('active')) {
+            setSeleccionado(e.target.innerText);
+            removeActive();
+            e.target.classList.add('active');
+        }
     }
     return (
         <div className="seleccionado">
             <div>
                 <h4>{seleccionado}</h4>
             </div>
-            <div className="botones">
-                <button className="boton ambiental" onClick={changeSeleccionado}>AMBIENTAL</button>
+            <div className="botones" ref={refBotones}>
+                <button className="boton ambiental active" onClick={changeSeleccionado} >AMBIENTAL</button>
                 <button className="boton economico" onClick={changeSeleccionado}>ECONÓMICO</button>
                 <button className="boton social" onClick={changeSeleccionado}>SOCIAL</button>
             </div>
